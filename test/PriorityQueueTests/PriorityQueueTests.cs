4deead5b19c314b54ec8df3503373131400736c8
bambiedjj <bonkatie@uw.edu>
2019-10-27 15:43:07 -0700

    Create XUnit project for PriorityQueue

---
+++ test/PriorityQueueTests/PriorityQueueTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PriorityQueueTests
+{
+    public class PriorityQueueTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
